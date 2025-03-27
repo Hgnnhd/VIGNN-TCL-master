c7f03cef6f2a10f91d34ea8b9ae0a3cc968e9c69
Hgnnhd <72909039+Hgnnhd@users.noreply.github.com>
2025-03-27 20:42:33 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -20,13 +20,12 @@ def get_num_units(
     return num_units
 
 def set_seed(seed_value):
-    """设置所有需要的种子以保证可复现性."""
-    random.seed(seed_value)  # Python内置随机库
-    np.random.seed(seed_value)  # Numpy库
-    torch.manual_seed(seed_value)  # PyTorch
+    random.seed(seed_value)  
+    np.random.seed(seed_value)  
+    torch.manual_seed(seed_value) 
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed_value)
-        torch.cuda.manual_seed_all(seed_value)  # 如果使用多个GPU
+        torch.cuda.manual_seed_all(seed_value)  
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.enabled = False
@@ -79,28 +78,8 @@ def imputation(data):
 
     data[nanIdx] = np.take([0] * 40, nanIdx[1])
 
-
-
     t = len(data) - 1
     row = list(data[t, :])
-
-    # for j in range(1, past_time):
-    #     if (t - j < 0):
-    #         row.extend([0] * 40)
-    #     else:
-    #         row.extend(data[t - j, :])
-    #
-    # row = np.array([row])
-    #
-    # return row
-
-    # for i in range(0, 39):
-    #     if i in [14, 15, 16, 19, 20, 22, 25, 26, 27, 30, 31, 32]:
-    #         data[:, i] = 10 * (np.log(data[:, i]) - varlogmeans[i]) / varlogstds[i]
-    #     else:
-    #         data[:, i] = 10 * (data[:, i] - varmeans[i]) / varstds[i]
-
-    # data = np.concatenate((data, delta), axis=1)
     data = np.concatenate((data, mask), axis=1)
 
 
@@ -154,32 +133,4 @@ def load_data(input_directory, files):
 
     return DATA, LABEL, patient_indices
 
-class CustomRandomUnderSampler:
-
-    def __init__(self, random_state=None):
-        self.random_state = random_state
-
-    def fit_resample(self, X, y):
-
-        random_state = check_random_state(self.random_state)
-
-        # 获取每个类别的样本数量
-        unique_classes, class_counts = np.unique(y, return_counts=True)
-
-        # 计算下采样后的目标样本数量（例如，取少数类的样本数量的最小值作为目标数量）
-        target_count = np.min(class_counts)
-
-        # 随机下采样每个类别的样本
-        resampled_data = []
-        for class_label in unique_classes:
-            class_indices = np.where(y == class_label)[0]
-            sampled_indices = random_state.choice(class_indices, target_count, replace=False)
-            resampled_data.append(X[sampled_indices])
-
-        # 合并并打乱样本
-        resampled_data = np.concatenate(resampled_data, axis=0)
-        # 创建对应的标签
-        y_resampled = np.repeat(unique_classes, target_count)
-
-        return resampled_data, y_resampled
 
